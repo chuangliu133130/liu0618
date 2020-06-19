182397e33fbad3f83c1771b564500f2f4abe9dfd
chuangliu133130 <chuangliu133130@126.com>
2020-06-19 12:20:23 +0800

    aaa

--- aa/aa/AppDelegate.m
+++ aa/aa/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog( @"fafaf");
+    NSLog(@"aaaaaaaaa222");
     return YES;
 }
 
